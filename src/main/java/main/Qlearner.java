89bb1ad7314b75a3ceeb356bb6fd1e43bb78a5ed
nakul shukla <nakulshukla@yahoo.com>
2015-11-07 13:10:05 -0500

    initial q learning

---
+++ src/main/java/main/Qlearner.java
@@ -0,0 +1,41 @@
+package main;
+
+import java.util.*;
+
+/**
+ * Created by Nakul on 11/6/2015.
+ */
+public class Qlearner {
+
+    private List<Protein> database;
+    private Protein[] population;
+    private double[] fitness;
+    private int populationSize;
+
+    public double protMass(char[] protBuff){
+        double mass = 0.0;
+        for(int i = 0; i < protBuff.length; i++){
+            if(protBuff[i] == 'A') mass += 71.03711;
+            else if(protBuff[i] == 'C') mass += 103.00919;
+            else if(protBuff[i] == 'D') mass += 115.02694;
+            else if(protBuff[i] == 'E') mass += 129.04259;
+            else if(protBuff[i] == 'F') mass += 147.06841;
+            else if(protBuff[i] == 'G') mass += 57.02146;
+            else if(protBuff[i] == 'H') mass += 137.05891;
+            else if(protBuff[i] == 'I') mass += 113.08406;
+            else if(protBuff[i] == 'K') mass += 128.09496;
+            else if(protBuff[i] == 'L') mass += 113.08406;
+            else if(protBuff[i] == 'M') mass += 131.04049;
+            else if(protBuff[i] == 'N') mass += 114.04293;
+            else if(protBuff[i] == 'P') mass += 97.05276;
+            else if(protBuff[i] == 'Q') mass += 128.05858;
+            else if(protBuff[i] == 'R') mass += 156.10111;
+            else if(protBuff[i] == 'S') mass += 87.03203;
+            else if(protBuff[i] == 'T') mass += 101.04786;
+            else if(protBuff[i] == 'V') mass += 99.06841;
+            else if(protBuff[i] == 'W') mass += 186.07931;
+            else if(protBuff[i] == 'Y') mass += 163.06333;
+        }
+        return mass;
+    }
+}
